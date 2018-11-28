148b2fbdca601ca24b4c238cac075cf525920e3d
JP Blanchette <empirecoder@gmail.com>
2018-11-28 13:15:36 -0500

    Object.key find example from Greg

--- pens/Object.keys_find_match/script.js
+++ pens/Object.keys_find_match/script.js
@@ -1,6 +1,8 @@
 // Object.keys find match
 console.clear();
 
+import find from "lodash/find";
+
 const unitId = "btyb-gpt-1";
 const isMatch = (id) => (unitId === id);
 
@@ -27,3 +29,19 @@ console.log("cats2 is: " + cats2.find((item) => {
   return item.value === true;
 }).name);
 
+// news below is not a type, per se, but a catch all for "none"/"column"
+const pgtype = "story";
+const subtype = "video";
+const types = {
+  longread: subtype === "longread",
+  video: subtype === "video",
+  gallery: subtype === "gallery",
+  news: pgtype === "story" && ["longread", "gallery", "interactive"].indexOf(subtype) === -1,
+  interactive: subtype === "interactive"
+};
+const contentType = find(Object.keys(types), val => types[val] === true);
+// const contentType = find(Object.keys(types), val => {
+//   console.log("val", val);
+//   return types[val] === true;
+// });
+console.log("contentType is: ", contentType);
